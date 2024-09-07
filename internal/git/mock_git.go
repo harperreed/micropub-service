85d7f8414cbd114dc647d64cff0eeb3f2596085e
Harper Reed <harper@nata2.org>
2024-09-06 23:31:40 -0500

    Refactor file path construction to use string conc

--- internal/git/mock_git.go
+++ internal/git/mock_git.go
@@ -1,133 +1,86 @@
 package git
 
 import (
-	"fmt"
-	"time"
-	"path/filepath"
-	"os"
-	"log"
+    "fmt"
+    "time"
+    "os"
 )
 
-// MockGitOperations is a mock implementation of the GitOperations interface for testing purposes
 type MockGitOperations struct {
-	Posts map[string]string // map to store posts with URL as key and content as value
+    DefaultGitOperations
 }
 
-// NewMockGitOperations creates a new instance of MockGitOperations
-func NewMockGitOperations() *MockGitOperations {
-	return &MockGitOperations{
-		Posts: make(map[string]string),
-	}
-}
+// Add these methods to your MockGitOperations struct
 
-// CreatePost simulates creating a new post
 func (m *MockGitOperations) CreatePost(content map[string]interface{}) error {
-	properties, ok := content["properties"].(map[string]interface{})
-	if !ok {
-		return fmt.Errorf("invalid properties")
-	}
+    properties := content["properties"].(map[string]interface{})
+    title := "Untitled Post"
+    if titleValue, ok := properties["title"]; ok {
+        if titleSlice, ok := titleValue.([]interface{}); ok && len(titleSlice) > 0 {
+            title, _ = titleSlice[0].(string)
+        }
+    }
+    filename := fmt.Sprintf("%s-%s.md", time.Now().Format("2006-01-02"), sanitizeFilename(title))
+    filePath := RepoPath + "/" + filename
 
-	title := "Untitled Post"
-	if titleValue, ok := properties["title"]; ok {
-		if titleSlice, ok := titleValue.([]interface{}); ok && len(titleSlice) > 0 {
-			title, _ = titleSlice[0].(string)
-		}
-	}
+    // Create the file
+    file, err := os.Create(filePath)
+    if err != nil {
+        return err
+    }
+    defer file.Close()
 
-	body := ""
-	if contentValue, ok := properties["content"]; ok {
-		if contentSlice, ok := contentValue.([]interface{}); ok && len(contentSlice) > 0 {
-			body, _ = contentSlice[0].(string)
-		}
-	}
+    // Write some content to the file
+    _, err = file.WriteString("Test content")
+    if err != nil {
+        return err
+    }
 
-	if body == "" {
-		return fmt.Errorf("missing content")
-	}
-
-	filename := fmt.Sprintf("%s-%s.md", time.Now().Format("2006-01-02"), sanitizeFilename(title))
-	url := fmt.Sprintf("/%s", filename)
-
-	m.Posts[url] = fmt.Sprintf("---\ntitle: %s\ndate: %s\n---\n\n%s", title, time.Now().Format(time.RFC3339), body)
-	content["url"] = url
-
-	log.Printf("Created post: %s", url)
-	return nil
+    content["url"] = fmt.Sprintf("/%s", filename)
+    return nil
 }
 
-// UpdatePost simulates updating an existing post
 func (m *MockGitOperations) UpdatePost(content map[string]interface{}) error {
-	url, ok := content["url"].(string)
-	if !ok {
-		return fmt.Errorf("invalid URL")
-	}
-
-	properties, ok := content["properties"].(map[string]interface{})
-	if !ok {
-		return fmt.Errorf("invalid properties")
-	}
-
-	existingContent, exists := m.Posts[url]
-	if !exists {
-		return fmt.Errorf("post not found")
-	}
-
-	frontmatter, oldContent, err := SplitFrontmatterAndContent(existingContent)
-	if err != nil {
-		return err
-	}
-
-	if title, ok := properties["title"].([]interface{}); ok && len(title) > 0 {
-		frontmatter["title"] = title[0]
-	}
-
-	if newContent, ok := properties["content"].([]interface{}); ok && len(newContent) > 0 {
-		oldContent = newContent[0].(string)
-	}
-
-	m.Posts[url] = CreateContentWithFrontmatter(frontmatter, oldContent)
-
-	log.Printf("Updated post: %s", url)
-	return nil
+    url, ok := content["url"].(string)
+    if !ok {
+        return fmt.Errorf("invalid URL")
+    }
+    filePath := RepoPath + "/" + url
+    if _, err := os.Stat(filePath); os.IsNotExist(err) {
+        return fmt.Errorf("file not found")
+    }
+    return nil
 }
 
-// DeletePost simulates deleting a post
 func (m *MockGitOperations) DeletePost(content map[string]interface{}) error {
-	url, ok := content["url"].(string)
-	if !ok {
-		return fmt.Errorf("invalid URL")
-	}
-
-	if _, exists := m.Posts[url]; !exists {
-		return fmt.Errorf("post not found")
-	}
-
-	delete(m.Posts, url)
-
-	log.Printf("Deleted post: %s", url)
-	return nil
+    url, ok := content["url"].(string)
+    if !ok {
+        return fmt.Errorf("invalid URL")
+    }
+    filePath := RepoPath + "/" + url
+    if _, err := os.Stat(filePath); os.IsNotExist(err) {
+        return fmt.Errorf("file not found")
+    }
+    return os.Remove(filePath)
 }
 
-// InitializeRepo simulates initializing a git repository
 func (m *MockGitOperations) InitializeRepo() error {
-	log.Println("Simulating repository initialization")
-	return nil
+    // Simulate initializing a repo
+    return nil
 }
 
-// gitAdd simulates adding a file to git
 func (m *MockGitOperations) gitAdd(filename string) error {
-	log.Printf("Simulating git add: %s", filename)
-	return nil
+    // Simulate git add
+    return nil
 }
 
-// gitCommit simulates committing changes to git
 func (m *MockGitOperations) gitCommit(message string) error {
-	log.Printf("Simulating git commit: %s", message)
-	return nil
+    // Simulate git commit
+    return nil
 }
 
-// gitPush simulates pushing changes to a remote repository
+// Override the gitPush method to simulate a push without actually running the command
 func (m *MockGitOperations) gitPush() error {
-	log.Println("Simulating git push")
-	return nil
+    fmt.Println("Simulating git push...")
+    return nil // Simulate a successful push
 }
